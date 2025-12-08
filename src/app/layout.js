d524ad47c769dd9c6bfd962020f4a956c92887c9
AnferneeDev <anfernee.developer@gmail.com>
2025-12-08 05:25:47 -0400

    style

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,8 +10,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {/* All the AIM LINE divs have been REMOVED from this file */}
-
         <main className="h-screen overflow-y-scroll">{children}</main>
 
         <Analytics />
